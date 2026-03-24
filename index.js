9317161dc230491d5b3f8b989aad861cbd1585d1
Abanou6Nassif <abanoub_133569@commerce.sohag.edu.eg>
2026-03-24 19:53:56 +0200

    Making small changes to the import statements in the index.js file

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-import dotenv from "dotenv"
-dotenv.config()
-
-const express = require("express");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 const app = express();
 
+app.use(express.json());
 
-app.use(express.json()); 
+const port = process.env.PORT;
 
-
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
