ed6d2ef1406dde0aca6c3903f1e918e3f321ac5e
mostafaeldabaaa <mostafamohamedeldabaa@gmail.com>
2026-03-25 10:54:31 +0200

    Connect to MongoDB and start server
    Refactor server startup to include MongoDB connection.

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
-const app = express();
 
+import express from "express";
+import { MongoClient } from "mongodb"; 
+
+const app = express();
 app.use(express.json());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+const uri =  "mongodb+srv://mostafaeldabaaa98:mostafa123456@cluster0.jx9irjt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const client = new MongoClient(uri);
+let database;
+
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("✅ Connected to MongoDB Atlas");
+
+    database = client.db("stack-overflow");
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (err) {
+    console.error(" Error connecting to DB", err);
+  }
+}
+
+startServer();;
