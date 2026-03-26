74ef218ff267cf81356ae1936f7b83735190e363
Shimaa Kadry <shimaakadry58@gmail.com>
2026-03-26 13:11:40 +0200

    Add User model schema

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
-import { MongoClient } from "mongodb"; 
+import mongoose from "mongoose";
+import dns from "node:dns/promises";
+
+dotenv.config();
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
 const app = express();
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
-const uri =  "mongodb+srv://mostafaeldabaaa98:mostafa123456@cluster0.jx9irjt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri);
-let database;
+
+const port = process.env.PORT;
+const uri =
+  "mongodb+srv://team2:guKhaFulSJAsAvWG@cluster0.i7u5kme.mongodb.net/stackOver?appName=Cluster0";
+
 
 async function startServer() {
   try {
-    await client.connect();
-    console.log(" Connected to MongoDB Atlas");
-
-    database = client.db("stack-overflow");
+    await mongoose.connect(uri);
+    console.log(" Connected to database");
 
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
@@ -28,4 +29,4 @@ async function startServer() {
   }
 }
 
-startServer();;
+startServer();
---
+++ model/user.model.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      minLength: 3,
+      required: true,
+    },
+    userNumber: {
+      type: Number,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/.test(value);
+        },
+        message: ({ value }) => `${value} Invalid Email`,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+    profilePhoto: {
+      type: String,
+      default: function () {
+        return `https://secure.gravatar.com/avatar/${this._id}?s=90&d=identicon`;
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  if (!this.userNumber) {
+    const User = this.constructor;
+    let isUnique = false;
+    let randomNumber;
+
+    while (!isUnique) {
+      randomNumber = Math.floor(100000 + Math.random() * 900000);
+
+      const existingUser = await User.findOne({ userNumber: randomNumber });
+      if (!existingUser) {
+        isUnique = true;
+      }
+    }
+
+    this.userNumber = randomNumber;
+  }
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel;
