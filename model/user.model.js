fcd066ef4333770fee13bf4b4b87b3d4fbb55675
Abanoub Nassif <abanoub_133569@commerce.sohag.edu.eg>
2026-03-27 20:19:12 +0200

    Change mongoose model name from 'users' to 'user'

--- model/user.model.js
+++ model/user.model.js
@@ -67,6 +67,6 @@ userSchema.pre("save", async function (next) {
   }
 });
 
-const userModel = mongoose.model("users", userSchema);
+const userModel = mongoose.model("user", userSchema);
 
 export default userModel;
